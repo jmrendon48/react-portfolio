c2711fee2a88181365f4988c1ddeed9c06912ef1
AlexDeLarge <jmrendon48@gmail.com>
2021-05-03 02:39:48 -0700

    fixed styling

--- src/components/Nav/index.js
+++ src/components/Nav/index.js
@@ -7,20 +7,20 @@ function Nav() {
 
   return (
     <header>
-      <h1>Joshua M. Rendon</h1>
+      <h1 className="white">Joshua M. Rendon</h1>
       <nav>
         <ul className="flex-row">
           <li className="mx-2">
-            <a href="#about">About Me</a>
+            <a href="#about" className="white">About Me</a>
           </li>
           <li className="mx-2">
-            <span onClick={categorySelected}>Portfolio</span>
+            <span onClick={categorySelected} className="white">Portfolio</span>
           </li>
           <li className="mx-2">
-            <span onClick={categorySelected}>Contact</span>
+            <span onClick={categorySelected} className="white">Contact</span>
           </li>
           <li className="mx-2">
-            <span onClick={categorySelected}>Resume</span>
+            <span onClick={categorySelected} className="white">Resume</span>
           </li>
         </ul>
       </nav>
